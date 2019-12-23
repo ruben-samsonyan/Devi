348994d1c4381bb0fe81289898874f673d4a5877
Ruben Samsonyan <ruben.samsonyan@auto1.com>
2019-12-23 01:20:03 +0100

    Initial Commit

---
+++ Devi/Devi.h
@@ -0,0 +1,19 @@
+//
+//  Devi.h
+//  Devi
+//
+//  Created by Ruben Samsonyan on 23.12.19.
+//  Copyright © 2019 Ruben Samsonyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Devi.
+FOUNDATION_EXPORT double DeviVersionNumber;
+
+//! Project version string for Devi.
+FOUNDATION_EXPORT const unsigned char DeviVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Devi/PublicHeader.h>
+
+
